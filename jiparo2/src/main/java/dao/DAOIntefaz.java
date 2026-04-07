f3f07b4bfa07a740702e6d189db777f8282cb1bf
Feodorovich18 <isaac.villanueva.valdes.wired@gmail.com>
2026-04-07 08:44:39 -0600

    Delete jiparo2 directory

--- jiparo2/src/main/java/dao/DAOIntefaz.java
+++
@@ -1,7 +0,0 @@
-package dao;
-
-public interface DAOIntefaz {
-    static final String URL = "jdbc:postgresql://localhost:5432/jiparo";
-    static final String USER = "postgres";
-    static final String PASSWORD = "Noexiste18";
-}
